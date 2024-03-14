a6bf0e453780561a0550103f4d710ac5fd9b207f
sankarshan07 <129412327+sankarshan07@users.noreply.github.com>
2024-03-14 20:19:33 +0530

    Update bgremover.py

--- bgremover.py
+++ bgremover.py
@@ -2,7 +2,7 @@ from rembg import remove
 import easygui
 from PIL import Image
 
-img =Image.open()   #add image 
+img =Image.open("")   #add image 
 output = remove(img)
 output.save("")  # add image name to be saved
 #
